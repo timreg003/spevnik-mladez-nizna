e030815f7b6c3635c0a59e937d2179a76feb2b7b
timreg003 <timotejreguly@gmail.com>
2026-01-18 23:45:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,41 @@
-const CACHE_NAME = 'spevnik-v1';
-const ASSETS = ['./', './index.html', './style.css', './script.js', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'];
+const CACHE_NAME = 'spevnik-v2'; // Zmenili sme verziu na v2, aby si iPhone vynútil aktualizáciu
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './icon.png',
+  './manifest.json',
+  // Tieto riadky prinútia iPhone uložiť ikony a písma do offline pamäte
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/webfonts/fa-solid-900.woff2',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Sťahujem súbory pre offline režim...');
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
